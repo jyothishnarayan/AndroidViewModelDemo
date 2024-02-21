e9c853d8e3fd4626d9dd56aeec24e50f3d8d0fb4
jyothishnarayan <jyothishn@gmail.com>
2024-02-21 10:28:49 +0530

    View Model Demo 1.0

---
+++ app/src/main/java/com/example/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.viewmodeldemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodeldemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.totalCount.observe(this, Observer {
+            binding.countText.text = it.toString()
+        })
+        //binding.countText.text = viewModel.getCurrentCount().toString()
+        binding.button.setOnClickListener {
+
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
+    }
+}
